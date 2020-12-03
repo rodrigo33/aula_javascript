092f1f7583fb06a747e259d8d1004d9bb2eb6005
rodrigo33 <rbarbosa.barbosa@gmail.com>
2020-12-02 22:32:58 -0300

    adiciona js na pagina html, operacoes com string, numeros e funcoes nativas do js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "rodrigo barbosa"
+var n1 = 32
+var n2 = 14
+var frase = "Flamengo melhor time"
+
+console.log(nome)
+console.log(n1 + n2)
+console.log(frase.replace("Flamengo", "Palmeiras"))
+console.log(frase.toLowerCase())
+
+//alert(frase.replace("Flamengo", "Palmeiras"))
